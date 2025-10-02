0ccd4c94c836160bbc36336991442ab66849211d
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-10-02 14:02:11 +0200

    S36:610 | added auth.component and template. implemented route /auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeService } from "./recipes/recipe.service";
+import { AuthComponent } from "./auth/auth.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    AuthComponent,
     DropdownDirective,
     HeaderComponent,
     RecipeDetailComponent,
